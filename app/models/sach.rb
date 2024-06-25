9c617f8d35c6a76aaa5a8f704aa2a52c1a0be070
wynkth283 <wynkth283@gmail.com>
2024-06-25 23:01:29 +0700

    Update

--- app/models/sach.rb
+++ app/models/sach.rb
@@ -4,12 +4,12 @@ class Sach < ApplicationRecord
   belongs_to :danh_muc
   def self.search(query)
     if query.to_i.to_s == query # Check if the query is a numeric string
-      where("id = ? OR TenSach LIKE ?", query.to_i, "%#{query}%")
+      where("id = ? OR tensach LIKE ?", query.to_i, "%#{query}%")
     else
-      where("TenSach LIKE ?", "%#{query}%")
+      where("tensach LIKE ?", "%#{query}%")
     end
   end
-  
+
   def self.filter_by_category(category_id)
     where(danh_muc_id: category_id)
   end
